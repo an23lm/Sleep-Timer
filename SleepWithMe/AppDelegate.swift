51a0ec283f9c6180addfb854e2d2b314ff978166
an23lm <anselmjosephs@gmail.com>
2018-06-05 01:14:33 +0530

    Initial Commit

---
+++ SleepWithMe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SleepWithMe
+//
+//  Created by Ansèlm Joseph on 05/06/18.
+//  Copyright © 2018 an23lm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SleepWithMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SleepWithMe
+//
+//  Created by Ansèlm Joseph on 05/06/18.
+//  Copyright © 2018 an23lm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
