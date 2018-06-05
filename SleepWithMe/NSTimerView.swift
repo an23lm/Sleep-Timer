b8f39106913c1cd9f617ab1fb73d9b5346816888
an23lm <anselmjosephs@gmail.com>
2018-06-05 05:34:24 +0530

    idk man im sleepy

---
+++ SleepWithMe/NSTimerView.swift
@@ -0,0 +1,19 @@
+//
+//  NSTimerView.swift
+//  SleepWithMe
+//
+//  Created by Ansèlm Joseph on 05/06/18.
+//  Copyright © 2018 an23lm. All rights reserved.
+//
+
+import Cocoa
+
+class NSTimerView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ SleepWithMe/SleepScript.scpt
@@ -0,0 +1,3 @@
+tell application "System Events"
+    sleep
+end tell
